b58b2c423ce6f5d21c5a25ea5d8e18e271848820
Daniel André <dap1995@gmail.com>
2016-11-07 11:24:05 -0200

    Added bodyParser, eslint reconfigured and commented http post

--- code/apps.js
+++ code/apps.js
@@ -1,16 +1,21 @@
-import express from "express";
+import express from 'express';
+import bodyParser from 'body-parser';
 //import dbconfig from "./dbconfig";
 //import mysql from "mysql";
 //var connection = mysql.createConnection(dbconfig);
-import { Detentos } from "./models/detentos";
-var app = express();
-var detentos = new Detentos();
-app.get("/", function (req, res) {
+import { Detentos } from './models/detentos';
+let app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+let detentos = new Detentos();
+app.get('/', function (req, res) {
   detentos.get(res);
 });
 
-app.post("/detentos/create", function(req, res){
+/*
+app.post('/detentos/create', function(req, res){
+  console.log(req.body);
   detentos.post(req.body, res);
 });
-
+*/
 app.listen(8082);
--- code/models/detentos.js
+++ code/models/detentos.js
@@ -1,23 +1,27 @@
-import dbconfig from "../dbconfig";
-import mysql from "mysql";
-var conn = mysql.createConnection(dbconfig);
+import dbconfig from '../dbconfig';
+import mysql from 'mysql';
+let conn = mysql.createConnection(dbconfig);
 
 export class Detentos {
 
   get(res){
-    conn.query("select * from Detentos", function(err, result){
+    conn.query('select * from Detentos', function(err, result){
       res.send(result);
     });
   }
-
+  
+/*
   post(detento, res){
-    conn.query("insert into Detentos set ?", detento, function(err, result){
+    console.log(detento);
+    conn.query('insert into Detentos (ind_id) values (?)', detento.ind_id, function(err, result){
       if (err) {
-        res.send(err);
+        console.error(err);
+        res.send({status: 1, message: 'Error! Detento not created.'});
       } else {
         console.log(result);
-        res.send({status: 0, message: "Detento created"});
+        res.send({status: 0, message: 'Detento created'});
       }
     });
-  }
+  }*/
+
 }
